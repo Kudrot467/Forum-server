b1e4ce45b9de82b3a330452807775bc5fa80d7a3
Kudrot467 <kkudrotekhoda@gmail.com>
2023-11-28 02:21:54 +0600

    manage users work added some part

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json())
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.clkkquk.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -26,7 +26,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    //await client.connect();
 
     const usersCollection= client.db('forumDB').collection('users');
     const postsCollection=client.db('forumDB').collection('posts');
@@ -47,6 +47,24 @@ async function run() {
         const post=req.body;
         const result=await postsCollection.insertOne(post);
         res.send(result);
+    })
+
+    app.patch('/users/admin/:id',async(req,res)=>{
+      const id=req.params.id;
+      const query={_id:new ObjectId(id)}
+      const updatedDoc={
+        $set:{
+          role:'admin'
+        }
+      }
+      const result=await usersCollection.updateOne(query,updatedDoc)
+    })
+
+    app.delete('/users/:id', async(req,res)=>{
+      const id=req.params.id;
+      const query={_id: new ObjectId(id)}
+      const result=usersCollection.deleteOne(query)
+      res.send(result);
     })
 
     app.get('/users',async(req,res)=>{
